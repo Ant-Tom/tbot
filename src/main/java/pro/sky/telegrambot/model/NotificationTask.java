4defcb536ad24b239c1a26303d2b43e82bc6c41b
Anton <146194876+Ant-Tom@users.noreply.github.com>
2024-12-10 11:56:21 +0300

    DZ

---
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -0,0 +1,53 @@
+package pro.sky.telegrambot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class NotificationTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long chatId;
+
+    private String message;
+
+    private LocalDateTime notifyAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getNotifyAt() {
+        return notifyAt;
+    }
+
+    public void setNotifyAt(LocalDateTime notifyAt) {
+        this.notifyAt = notifyAt;
+    }
+}
+
